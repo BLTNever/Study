77ffba59020289f83eef807dc97982b4d96aefb3
pengfei.zhang <pengfei.zhang@sao365.cn>
2017-03-30 10:13:16 +0800

    @bypass

---
+++ ReactGulp/my-app/src/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Test.css';
+
+class Test extends Component {
+    render() {
+        return (
+            <div className="Text">
+                This is awesome!
+            </div>
+        )
+    }
+}
+
+export default Test;
